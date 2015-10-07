7dbc7afb92a8a721bcf5c10b3660d006f00e0ee1
Andela Developer <andeladev@users-MacBook-Pro-3.local>
2015-10-07 09:45:07 +0100

    fragment caching

--- app/helpers/author_helper.rb
+++ app/helpers/author_helper.rb
@@ -1,8 +1,10 @@
 module AuthorHelper
   def cache_key_for_authors
     count          = Author.count
+    article_count = Article.count
     max_updated_at = Author.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "authors/all-#{count}-#{max_updated_at}"
+    updated_at = Article.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "authors/all-#{count}-#{max_updated_at}-#{article_count}-#{updated_at}"
   end
-  
+
 end
