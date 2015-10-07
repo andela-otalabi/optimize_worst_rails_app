7959588a20a3af45f2942f2a2bbb99d4df8a836c
Andela Developer <andeladev@users-MacBook-Pro-3.local>
2015-10-07 10:13:59 +0100

    cache keys

--- app/helpers/author_helper.rb
+++ app/helpers/author_helper.rb
@@ -2,9 +2,9 @@ module AuthorHelper
   def cache_key_for_authors
     count          = Author.count
     article_count = Article.count
-    max_updated_at = Author.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    updated_at = Article.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "authors/all-#{count}-#{max_updated_at}-#{article_count}-#{updated_at}"
+    author_max_updated_at = Author.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    article_max_updated_at = Article.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "authors/all-#{count}-#{author_max_updated_at}-#{article_count}-#{article_max_updated_at}"
   end
 
 end
