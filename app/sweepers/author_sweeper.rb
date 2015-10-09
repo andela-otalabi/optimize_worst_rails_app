eb1b5e3dfd8667debf206a60aff71eeb618641b4
Andela Developer <andeladev@users-MacBook-Pro-3.local>
2015-10-09 17:03:14 +0100

    sweep cache after delete

--- app/sweepers/author_sweeper.rb
+++ app/sweepers/author_sweeper.rb
@@ -5,6 +5,10 @@ class AuthorSweeper < ActionController::Caching::Sweeper
     expire_cache_for(author)
   end
 
+  def after_destroy(author)
+    expire_cache_for(author)
+  end
+
   private
   def expire_cache_for(author)
     expire_fragment(:controller => '#{record}', 
